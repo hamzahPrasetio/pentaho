a20799271aec610fb370dd38925af3d470eb600c
hamzahPrasetio <prasetiohamzah@gmail.com>
2021-12-10 16:31:56 +0700

    Initial commit

---
+++ src/main/java/com/example/pentaho/PentahoApplication.java
@@ -0,0 +1,14 @@
+package com.example.pentaho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PentahoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PentahoApplication.class, args);
+		new RunningTransformations().runTransformationFromFileSystem("D:/Job/Export/tesrest.ktr");
+	}
+
+}
---
+++ src/main/java/com/example/pentaho/PentahoController.java
@@ -0,0 +1,5 @@
+package com.example.pentaho;
+
+public class PentahoController {
+
+}
