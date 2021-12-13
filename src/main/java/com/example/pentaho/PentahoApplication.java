84f41b391e1b1bbf8b884207c0acf5ef6ebd8ae0
hamzahPrasetio <prasetiohamzah@gmail.com>
2021-12-13 10:32:52 +0700

    ganti versi, karena cenah error di versi baru. sekarang ga ada error tp ga jaalan

--- src/main/java/com/example/pentaho/PentahoApplication.java
+++ src/main/java/com/example/pentaho/PentahoApplication.java
@@ -2,13 +2,15 @@ package com.example.pentaho;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.pentaho.di.core.logging.KettleLogStore;
 
 @SpringBootApplication
 public class PentahoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PentahoApplication.class, args);
-		new RunningTransformations().runTransformationFromFileSystem("D:/Job/Export/tesrest.ktr");
+		KettleLogStore.init();
+		new RunningTransformations().runTransformationFromFileSystem("D:\\Job\\Export\\tesrest.ktr");
 	}
 
 }
