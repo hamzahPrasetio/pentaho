4014f2cf3857f6c2f3f0719e276e28fe019daa32
hamzahPrasetio <prasetiohamzah@gmail.com>
2021-12-16 09:15:23 +0700

    add verification if the output file and its folder is created already in which it won't create a new one

--- src/main/java/com/example/pentaho/PentahoApplication.java
+++ src/main/java/com/example/pentaho/PentahoApplication.java
@@ -8,7 +8,10 @@ import org.pentaho.di.trans.Trans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.pentaho.di.core.logging.KettleLogStore;
+import org.w3c.dom.ls.LSOutput;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 
 @SpringBootApplication
@@ -17,20 +20,36 @@ public class PentahoApplication {
 	public static RunningTransformations instance;
 
 	public static void main(String[] args) {
+		// set variable n parameter
+		HashMap<String, String> parameters = new HashMap<String, String>();
+		parameters.put("Limit", "7");
+		parameters.put("filename", "testing/tesfilename");
+//		parameters.forEach((key, value) -> {
+//			System.out.println(key + " - " + value);
+//		});
+
 		SpringApplication.run(PentahoApplication.class, args);
+
+		if (Files.exists(Paths.get("testing"))) {
+			System.out.println("testing folder exist");
+		} else {
+			System.out.println("parantly testing doesnt exit");
+		}
+
+		if (Files.exists(Paths.get(parameters.get("filename") + ".txt"))) {
+			System.out.println("file output already exists");
+			return;
+		}
+
 		KettleLogStore.init();
 		try {
 			KettleEnvironment.init();
 		} catch (KettleException e) {
 			e.printStackTrace();
 		}
+
 		// Create an instance of this demo class for convenience
 		instance = new RunningTransformations();
-
-		// set variable n parameter
-		HashMap<String, String> parameters = new HashMap<String, String>();
-		parameters.put("Limit","7");
-		parameters.put("filename","tesfilename");
 
 		// run a transformation from the file system
 		Trans trans = instance.runTransformationFromFileSystem( "ktr/test.ktr", parameters );
@@ -46,7 +65,6 @@ public class PentahoApplication {
 		System.out.println( logText );
 		System.out.println( "END OF LOG REPORT" );
 		System.out.println( "************************************************************************************************" );
-
 
 		// run a transformation from the repository
 		// NOTE: before running the repository example, you need to make sure that the
